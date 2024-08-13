bd600125caac62ce31f9fd60995682d823f1a3da
Daniel88dev <daniel.hrynusiw@gmail.com>
2024-08-13 17:01:02 +0200

    finish task5 get reviews for isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -97,10 +97,23 @@ public_users.get("/title/:title", function (req, res) {
   }
 });
 
+//task 5
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  try {
+    const isbnRequested = req.params.isbn; // get ISBN from request params
+    const book = books[isbnRequested];
+
+    if (book) {
+      const reviews = book.reviews;
+      res.json(reviews); // Send the book reviews as JSON
+    } else {
+      res.status(404).json({ message: "Book not found" }); // Handle book not found
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving reviews" }); // Handle unknown errors
+  }
 });
 
 module.exports.general = public_users;
