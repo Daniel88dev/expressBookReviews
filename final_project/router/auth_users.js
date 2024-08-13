2f690a9ca65754a3b47c7f225ab437b79023cca8
Daniel88dev <daniel.hrynusiw@gmail.com>
2024-08-13 18:04:50 +0200

    finish task9 delete a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -76,6 +76,35 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+//task 9
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  try {
+    const requestedIsbn = req.params.isbn;
+    const username = req.session.authorization.username; // get username from session
+
+    if (!username) {
+      return res.status(401).json({ message: "Unauthorized" }); // Handle unauthorized
+    }
+
+    const book = books[requestedIsbn];
+
+    if (book) {
+      if (book.reviews[username]) {
+        // Check if exists review for the user
+        delete book.reviews[username]; // Delete the user review
+        res.json({ message: "Review deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Review not found" }); // Handle review not found
+      }
+    } else {
+      res.status(404).json({ message: "Book not found" }); // Handle book not found
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting review" }); // Handle unknown errors
+  }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
